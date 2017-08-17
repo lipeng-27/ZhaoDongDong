60c55173b36cb23314024cf12239104a3e9b6e82
PhilipHomePC <PhilipHomePC@PhilipHomePCdeiMac.local>
2017-08-17 16:29:07 +0800

    Initial Commit

---
+++ ZhaoDongDong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhaoDongDong
+//
+//  Created by PhilipHomePC on 2017/8/17.
+//  Copyright © 2017年 PhilipHomePC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
